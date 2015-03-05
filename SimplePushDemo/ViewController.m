b7dc64119e4ef1d5e0281f150eb2af3831e8aa19
j r conlin <jrconlin@mini.local>
2015-03-04 21:33:20 -0800

    Strip copyright, add MPL

--- SimplePushDemo/ViewController.m
+++ SimplePushDemo/ViewController.m
@@ -2,9 +2,11 @@
 //  ViewController.m
 //  SimplePushDemo
 //
+
 //  Created by mozilla on 2/17/15.
-//  Copyright (c) 2015 mozilla. All rights reserved.
-//
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #import "ViewController.h"
 #import "SRWebSocket.h"
